1f12fe45e207a6b4f4ed0f734e46970cb5f40da4
Samuel Daza <samueldazadaza@gmail.com>
2021-07-18 17:33:17 -0500

    Conectar la función con la descripción de personajes12/16

--- src/utils/getData.js
+++ src/utils/getData.js
@@ -1,9 +1,9 @@
 const API = 'https://rickandmortyapi.com/api/character/';
 
 const getData = async (id) => {
-    const apiUrl = id ? `${API}${id}` : API;
+    const apiURl = id ? `${API}${id}` : API;
     try {
-        const response = await fetch(apiUrl);
+        const response = await fetch(apiURl);
         const data = await response.json();
         return data;
     }   catch (error) {
